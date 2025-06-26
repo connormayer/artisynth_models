425ff43c91bdfacd8c63e7536a01bd1a72f2b733
mashachernets <achernet@student.ubc.ca>
2025-06-25 22:43:07 -0700

    Add Model + Batchworker to JawTongue
    This is the working Model and Batchworker that I made.

--- src/artisynth/models/jawTongue/batch/bracingBatchDriver.py
+++ src/artisynth/models/jawTongue/batch/bracingBatchDriver.py
@@ -1,12 +1,12 @@
 '''
 # @author: Connor Mayer
 '''
-from artisynth.models.jawTongue import JawHyoidFemMuscleTongueBatchWorker
+from artisynth.models.jawTongue import BadinJawHyoidTonguePositionBatchWorker
 from jarray import array
 import sys
 
 print(sys.argv)
 args = array(sys.argv, String)
 print(args)
-worker = JawHyoidFemMuscleTongueBatchWorker(args)
+worker = BadinJawHyoidTonguePositionBatchWorker(args)
 worker.run()
