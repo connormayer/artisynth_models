c13fe7903e4803ae2c00e5e0d49139eb843eb751
Krobusts <Kevinwang154@gmail.com>
2025-11-30 22:29:35 -0800

    Update BadinJawHyoidTonguePositionBatchWorker.java
    Updated Batchworker for Deactivated model

--- src/artisynth/models/jawTongue/BadinJawHyoidTonguePositionBatchWorker.java
+++ src/artisynth/models/jawTongue/BadinJawHyoidTonguePositionBatchWorker.java
@@ -60,7 +60,7 @@ public class BadinJawHyoidTonguePositionBatchWorker extends SimpleTimedBatchWork
    protected double mySettleTime;
    protected double myMaxTime;
    protected String myName = "default";
-   protected BadinJawHyoidTonguePositionActivated root;
+   protected BadinJawHyoidTonguePositionDeactivated root;
    protected FemMuscleModel face;
    protected FemMuscleModel tongue;
    protected ComponentList<MuscleExciter> exciters;
@@ -79,7 +79,7 @@ public class BadinJawHyoidTonguePositionBatchWorker extends SimpleTimedBatchWork
       myMaxTime = 1.00;
       mySettleTime = 0.40;
       
-      root = (BadinJawHyoidTonguePositionActivated) Main.getMain().getRootModel();
+      root = (BadinJawHyoidTonguePositionDeactivated) Main.getMain().getRootModel();
 
       
       exciters = (ComponentList<MuscleExciter>) root.findComponent("models/jawmodel/models/tongue/exciters");
@@ -102,7 +102,7 @@ public class BadinJawHyoidTonguePositionBatchWorker extends SimpleTimedBatchWork
    
    @Override
    protected void preSim() {
-      root = (BadinJawHyoidTonguePositionActivated) Main.getMain().getRootModel();
+      root = (BadinJawHyoidTonguePositionDeactivated) Main.getMain().getRootModel();
       tongue = root.getTongue();
       exciters = (ComponentList<MuscleExciter>) root.findComponent("models/jawmodel/models/tongue/exciters");
       jawOpenerExciter = (MuscleExciter) root.findComponent("models/jawmodel/exciters/bi_open");
